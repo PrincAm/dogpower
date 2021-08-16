27f406a445cf20b63537ccc8a75114053be4e57e
Adam <ablazek@securityscorecard.io>
2021-08-16 14:10:38 +0200

    feat: update site config

--- site-config.js
+++ site-config.js
@@ -4,13 +4,15 @@ module.exports = {
   siteTitle: 'Trénink psů po dobrém v Praze',
   siteTitleShort: 'Trénink psů',
   siteDescription: 'Trénink psů po dobrém v Praze',
-  siteUrl: 'https://www.dogpower.cz/',
-  themeColor: '#000',
+  siteUrl: 'https://stoic-wescoff-fb2417.netlify.app/', // TODO update site url
+  themeColor: '#222222',
   backgroundColor: '#fff',
   pathPrefix: null,
   logo: path.resolve(__dirname, 'src/images/icon.png'),
+  /*
   social: {
     twitter: 'gatsbyjs',
     fbAppId: '966242223397117',
   },
+  */
 };
