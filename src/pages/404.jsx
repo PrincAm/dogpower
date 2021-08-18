313f00cd464449218876ac2fe93e65fe7bb4ced8
Adam <ablazek@securityscorecard.io>
2021-08-18 16:06:35 +0200

    feat: update 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,8 +1,9 @@
 import React, { useContext } from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
 
 import Layout from 'components/Layout';
 import Text, { PageTitle } from 'components/Text';
-import { PageBox } from 'components/Box';
+import { PageBox, ImageBox } from 'components/Box';
 import ThemeContext from '../store/theme';
 
 const NotFound = () => {
@@ -11,6 +12,15 @@ const NotFound = () => {
     <Layout>
       <PageBox theme={theme}>
         <PageTitle>Nenašli jsme, co jste hledali :(</PageTitle>
+        <ImageBox>
+          <StaticImage
+            src="../../content/images/pages/404/404.jpg"
+            alt="collie"
+            placeholder="blurred"
+            layout="constrained"
+            style={{ borderRadius: '10px', maxHeight: '30rem' }}
+          />
+        </ImageBox>
         <Text as="p">Zkuste jinou stránku z menu.</Text>
       </PageBox>
     </Layout>
